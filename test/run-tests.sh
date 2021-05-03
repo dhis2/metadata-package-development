1371da63ac29bc18377560844c8474afc7ccf2c4
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-05-03 15:00:12 +0200

    print status

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
 url=$URL
+MERGE_MODE_STATUS="NOT_EXECUTED"
+REPLACE_MODE_STATUS="NOT_EXECUTED"
+
+
+print_status() {
+  divider=======================
+  divider=$divider$divider
+  header="\n %-10s %15s\n"
+  format=" %-10s %15s\n"
+  width=43
+
+  printf "$header" "TEST" "RESULT"
+  printf "%$width.${width}s\n" "$divider"
+  printf "$format" "MERGE"  "$MERGE_MODE_STATUS" 
+  printf "$format" "REPLACE" "$REPLACE_MODE_STATUS"
+  printf "%$width.${width}s\n" "$divider"
+}
+
 if [ -z "$url" ]; then
     echo "Please provide URL to dhis2 instance."
     exit 1
 fi
 
 if ./api-test.sh -f tests.json -url $url test merge_import; then 
-  echo "Import with merge mode passed."
+  MERGE_MODE_STATUS="PASSED"
 else 
-  echo "Import with merge mode failed. Trying replace mode.."
+  MERGE_MODE_STATUS="FAILED"
   if ./api-test.sh -f tests.json -url $url test replace_import; then 
-    echo "Import with replace mode passed. "
+    REPLACE_MODE_STATUS="PASSED"
 
   else 
-    echo "Import with replace mode failed. "
+    REPLACE_MODE_STATUS="FAILED"
+    print_status;
     exit 1
   fi
 fi
+
+print_status;
