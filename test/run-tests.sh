01ee8ebc7103a47a5edac34cdd832787fe264133
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-05-03 12:28:26 +0200

    fix: adds URL variable

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if ./api-test.sh -f tests.json -url http://localhost:8080/api test merge_import; then 
+url=$URL
+if [ -z "$url" ]; then
+    echo "Please provide URL to dhis2 instance."
+    exit 1
+fi
+
+if ./api-test.sh -f tests.json -url $url test merge_import; then 
   echo "Import with merge mode passed."
 else 
   echo "Import with merge mode failed. Trying replace mode.."
-  if ./api-test.sh -f tests.json -url http://localhost:8080/api test replace_import; then 
+  if ./api-test.sh -f tests.json -url $url test replace_import; then 
     echo "Import with replace mode passed. "
 
   else 
