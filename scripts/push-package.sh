7074bbf54099a2403545b3d3d63474c39a1e1095
radnov <radnov@pm.me>
2022-05-20 18:16:57 +0300

    refactor: Add custom commit message option and update script [DEVOPS-167]

--- scripts/push-package.sh
+++ scripts/push-package.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 
 file="$1"
 
-prefix="$(cat $file | jq -r '.package .code')"
-version="$(cat $file | jq -r '.package .DHIS2Version' | cut -d '.' -f 1,2)"
+package_prefix="$(cat $file | jq -r '.package .code')"
+version_branch="$(cat $file | jq -r '.package .DHIS2Version' | cut -d '.' -f 1,2)"
 
-package_code="${prefix:0:4}"
-sub_package_code="${prefix:4:2}"
+package_code="${package_prefix:0:4}"
+sub_package_code="${package_prefix:4:2}"
 
 repository_name=$(
   curl -fsSL "https://api.github.com/orgs/dhis2-metadata/repos?per_page=100" |
@@ -17,15 +17,20 @@ repository_name=$(
 
 repository_url="https://$GITHUB_CREDS_PSW@github.com/dhis2-metadata/$repository_name"
 
+commit_message="feat: Update $package_prefix package"
+if [[ "${Custom_commit_message:-}" ]]; then
+  commit_message="${Custom_commit_message}"
+fi
+
 git config --global user.email "$GITHUB_EMAIL"
 git config --global user.name "$GITHUB_CREDS_USR"
 
 git clone "$repository_url"
 cd "$WORKSPACE/$repository_name"
 
-git checkout "test-$version"
+git checkout "$version_branch"
 mkdir -p "$sub_package_code" && cp "$WORKSPACE/$file" "$sub_package_code/metadata.json"
 git add .
 
-git commit -m "Some message ..."
+git commit -m "$commit_message"
 git push "$repository_url"
