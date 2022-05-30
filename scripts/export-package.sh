cb3b865c75a49aeb34b8543b62e0410dbb292950
radnov <radnov@pm.me>
2022-05-30 16:47:12 +0300

    chore: Remove commented-out code [DEVOPS-167]

--- scripts/export-package.sh
+++ scripts/export-package.sh
@@ -22,36 +22,6 @@ case "$DHIS2_version" in
     ;;
 esac
 
-#if [[ "${Instance_url:-}" ]]; then
-#  instance="$Instance_url"
-#else
-#  if [[ "$type" == "TRK" || "$type" == "EVT" ]]; then
-#    case "$DHIS2_version" in
-#      "2.36")
-#        instance="$base_version_host/tracker_dev"
-#        ;;
-#      "2.37")
-#        instance="$new_version_host/tracker_dev237"
-#        ;;
-#      "2.38")
-#        instance="$new_version_host/tracker_dev238"
-#        ;;
-#    esac
-#  else
-#    case "$DHIS2_version" in
-#      "2.36")
-#        instance="$base_version_host/dev"
-#        ;;
-#      "2.37")
-#        instance="$new_version_host/dev237"
-#        ;;
-#      "2.38")
-#        instance="$new_version_host/dev238"
-#        ;;
-#    esac
-#  fi
-#fi
-
 pip3 install -r dhis2-utils/tools/dhis2-package-exporter/requirements.txt
 
 python3 -u dhis2-utils/tools/dhis2-package-exporter/package_exporter.py "$type" "$code" "$code" -desc="$description" -i="$instance"
