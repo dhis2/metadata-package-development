60300b007a3696cbddf4d046b3432714a510de3a
radnov <radnov@pm.me>
2022-06-20 15:23:43 +0300

    refactor: Make DHIS2 ver optional when selecting extraction instance [DEVOPS-167]

--- scripts/export-package.sh
+++ scripts/export-package.sh
@@ -5,22 +5,26 @@ set -euxo pipefail
 code="$1"
 type="$2"
 description="$3"
-base_version_host="$4"
+export_instance="$4"
 
 # Use the "path" of the base version host.
-new_version_host="https://who-dev.dhis2.org/${base_version_host##*/}"
+new_version_export_instance="https://who-dev.dhis2.org/${export_instance##*/}"
 
-case "$DHIS2_version" in
-  "2.36")
-    instance="$base_version_host"
-    ;;
-  "2.37")
-    instance="${new_version_host}237"
-    ;;
-  "2.38")
-    instance="${new_version_host}238"
-    ;;
-esac
+if [[ "${DHIS2_version:-}" ]]; then
+  case "$DHIS2_version" in
+    "2.36")
+      instance="$export_instance"
+      ;;
+    "2.37")
+      instance="${new_version_export_instance}237"
+      ;;
+    "2.38")
+      instance="${new_version_export_instance}238"
+      ;;
+  esac
+else
+  instance="$export_instance"
+fi
 
 pip3 install -r dhis2-utils/tools/dhis2-package-exporter/requirements.txt
 
